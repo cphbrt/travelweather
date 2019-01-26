ee171d9939635b8c2699f1a46e76b276d9a2984c
abinks <allie.a.binkley@gmail.com>
2019-01-26 13:29:20 -0600

    weather at start mid and end

--- weather.py
+++ weather.py
@@ -1,26 +1,102 @@
 from darksky import forecast
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+import os
+import googlemaps
+import polyline
 
-file = open('index.html', 'r')
-coordinates = file.readlines()[-5]
-file.close()
+#### INCOMING JSON
+incoming_dict = {
+    "env": "prod",
+    "start_location": "Nashville, TN",
+    "end_location": "San Diego, CA"
+    
+}
 
-print (coordinates)
+#### GOOGLE MAPS 
+mapApiKey = os.getenv('MAP_API_KEY')
+gmaps = googlemaps.Client(key=mapApiKey)
+start_loc = gmaps.geocode(incoming_dict["start_location"])
+end_loc = gmaps.geocode(incoming_dict["end_location"])
 
-# lat = open('index.html', 'r').read()
-# log = 
-# LOCATION = lat, log
+#### get lat/long
+start_lat =  start_loc[0]['geometry']['location']['lat']
+start_long = start_loc[0]['geometry']['location']['lng']
 
-# API_KEY='848314873d786d02ace0c34bfc27c484'
+end_lat =  end_loc[0]['geometry']['location']['lat']
+end_long = end_loc[0]['geometry']['location']['lng']
 
-# weekday = date.today()
-# with forecast(API_KEY, *LOCATION) as location:
-#     print(location.daily.summary, end = '\n---\n')
-#     for day in location.daily:
-#         day = dict(day = date.strftime(weekday, '%a'),
-#                    sum = day.summary,
-#                    tempMin = day.temperatureMin,
-#                    tempMax = day.temperatureMax
-#                    )
-#         print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
-#         weekday += timedelta(days=1)
+# get directions 
+directions_response = gmaps.directions(
+    incoming_dict["start_location"], 
+    incoming_dict["end_location"], 
+    mode="driving", 
+    departure_time=datetime.now()
+)
+
+# get all the points along the first route returned
+points = polyline.decode(directions_response[0]["overview_polyline"]["points"])
+
+# get the middle point
+lat, lng = points[int(len(points) / 2)]
+
+#### DARK SKY 
+skyApiKey = os.getenv('SKY_API_KEY')
+
+### coordiate pairs
+start_coordinates = start_lat, start_long
+mid_coordinates = lat, lng
+end_coordinates = end_lat, end_long
+
+# get mid directions 
+middirections_response = gmaps.directions(
+    incoming_dict["start_location"], 
+    "{},{}".format(lat, lng), 
+    mode="driving", 
+    departure_time=datetime.now()
+)
+
+### get forcast
+start_forecast = forecast(skyApiKey, *start_coordinates)
+mid_forecast = forecast(skyApiKey, *mid_coordinates)
+end_forecast = forecast(skyApiKey, *end_coordinates)
+
+start_weather = start_forecast.hourly[0].summary
+start_temp = start_forecast.currently.temperature
+
+### !!!!!!!!!!!!!! get time from maps?
+mid_arrival_sec = middirections_response[0]["legs"][0]["duration"]["value"]
+mid_arrival = int(round(mid_arrival_sec/60/60))
+
+mid_weather = mid_forecast.hourly[mid_arrival].summary
+mid_temp = mid_forecast.hourly[mid_arrival].temperature
+
+### !!!!!!!!!!!!!! get time from maps?
+end_arrival_sec = directions_response[0]["legs"][0]["duration"]["value"]
+end_arrival = int(round(end_arrival_sec/60/60))
+
+
+
+end_weather = end_forecast.hourly[end_arrival].summary
+end_temp = end_forecast.hourly[end_arrival].temperature
+
+### !!!!!!!!!!!!!! build array with "locations" ?
+
+outgoing_dict = {
+        "hourly": [
+            {   "location": "start",
+                "weather": start_weather,
+                "temp": start_temp,
+            },
+            {   "location": "mid",
+                "weather": mid_weather,
+                "temp": mid_temp,
+            },
+            {
+                "location": "end",
+                "weather": end_weather,
+                "temp": end_temp
+            }
+        ]
+    }
+
+print (outgoing_dict)

9aca2e2aa2dcfce86f92139955bd850975d62dbb
abinks <allie.a.binkley@gmail.com>
2019-01-26 08:20:14 -0600

    weather.py

--- weather.py
+++ weather.py
@@ -1,18 +1,26 @@
 from darksky import forecast
 from datetime import date, timedelta
 
-LOCATION = 35.846621999999996, -86.36526959999999
+file = open('index.html', 'r')
+coordinates = file.readlines()[-5]
+file.close()
 
-API_KEY='848314873d786d02ace0c34bfc27c484'
+print (coordinates)
 
-weekday = date.today()
-with forecast(API_KEY, *LOCATION) as location:
-    print(location.daily.summary, end = '\n---\n')
-    for day in location.daily:
-        day = dict(day = date.strftime(weekday, '%a'),
-                   sum = day.summary,
-                   tempMin = day.temperatureMin,
-                   tempMax = day.temperatureMax
-                   )
-        print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
-        weekday += timedelta(days=1)
+# lat = open('index.html', 'r').read()
+# log = 
+# LOCATION = lat, log
+
+# API_KEY='848314873d786d02ace0c34bfc27c484'
+
+# weekday = date.today()
+# with forecast(API_KEY, *LOCATION) as location:
+#     print(location.daily.summary, end = '\n---\n')
+#     for day in location.daily:
+#         day = dict(day = date.strftime(weekday, '%a'),
+#                    sum = day.summary,
+#                    tempMin = day.temperatureMin,
+#                    tempMax = day.temperatureMax
+#                    )
+#         print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
+#         weekday += timedelta(days=1)
